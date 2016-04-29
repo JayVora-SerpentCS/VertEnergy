41bb14abb299e9a7ea431d2044a9243d10ab6906
Vishal (SerpentCS) <support@serpentcs.com>
2016-04-29 21:07:35 +0530

    [ADD]Added web_digital_sign module

---
+++ web_digital_sign/__openerp__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 OpenERP SA (<http://www.openerp.com>)
+#    Copyright (C) 2011-2016 Serpent Consulting Services Pvt. Ltd.
+#    (<http://www.serpentcs.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Web Digital Signature",
+    "version": "1.1",
+    "author": "Serpent Consulting Services Pvt. Ltd.",
+    "category": 'web',
+    'complexity': "easy",
+    'depends': ['web'],
+    "description": """
+        This module provides the functionality to store digital signature
+        image for a record. The example can be seen into the User's form
+        view where we have added a test field under signature.
+    """,
+    'data': [
+        'views/web_digital_sign_view.xml',
+        'views/res_users_view.xml',
+    ],
+    'website': 'http://www.serpentcs.com',
+    'qweb': ['static/src/xml/digital_sign.xml'],
+    'installable': True,
+    'auto_install': False,
+}
