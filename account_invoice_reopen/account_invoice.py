b5795dd2b7409f51939345a4ee769d916728ef89
Vishal (SerpentCS) <support@serpentcs.com>
2016-05-05 18:04:52 +0530

    [IMP]account_invoice_reopen:Improve code as per pep8 coding standar and resolve error which occur during installation and testing

--- account_invoice_reopen/account_invoice.py
+++ account_invoice_reopen/account_invoice.py
@@ -32,14 +32,11 @@ class account_invoice(models.Model):
     _inherit = "account.invoice"
 
 #   def _get_state(self, cr, uid, ids, context=None):
-
 #       res = list(super(account_invoice, self)._columns['state'].selection)
 #       res.append(('draft_reset','Reset to draft'))
-
 #       return res
-
-#   _columns ={
-# FIXME the _get_state raises error
+#    _columns ={
+#    FIXME the _get_state raises error
 #        'state': fields.selection(selection=_get_state, string='State',
 #                    required=True),
 #       'state': fields.selection([
@@ -56,9 +53,9 @@ class account_invoice(models.Model):
 #           \n* The \'Pro-forma\' when invoice is in Pro-forma state,
 #                    invoice does not have an invoice number. \
 #           \n* The \'Open\' state is used when user create invoice,a invoice
-#       number is generated.Its in open state till user does not pay invoice. \
+#         number is generated.Its in open state till user does not pay invoice.
 #           \n* The \'Paid\' state is set automatically when the invoice
-#        is paid. Its related journal entries may or may not be reconciled. \
+#            is paid. Its related journal entries may or may not be reconciled.
 #           \n* The \'Cancelled\' state is used when user cancel invoice.'),
 
 #   }
@@ -68,7 +65,7 @@ class account_invoice(models.Model):
 #                         set state = 'active'
 #                       where state = 'complete'
 #                         and res_type = 'account.invoice'
-#""")
+#            """)
 
     @api.v7
     def action_reopen(self, cr, uid, ids, context=None):
@@ -83,7 +80,7 @@ class account_invoice(models.Model):
         invoices = self.read(cr, uid, ids, ['move_id', 'payment_ids', 'id'])
         _logger.debug('FGF reopen invoices %s' % (invoices))
 
-        #wf_service = netsvc.LocalService("workflow")
+#        wf_service = netsvc.LocalService("workflow")
 
         move_ids = []  # ones that we will need to update
         now = ' ' + _('Invalid') + time.strftime(' [%Y%m%d %H%M%S]')
@@ -94,9 +91,9 @@ class account_invoice(models.Model):
                 for move in account_move_obj.browse(cr, uid, move_ids):
                     if not move.journal_id.reopen_posted:
                         raise except_orm(_('Error !'),
-                                         _('You can not reopen invoice of '\
-                                           'this journal [%s]! You need to '\
-                                           'set Allow Update Posted Entries'\
+                                         _('You can not reopen invoice of '
+                                           'this journal [%s]! You need to '
+                                           'set Allow Update Posted Entries '
                                            'first') % (move.journal_id.name))
 
             if i['payment_ids']:
@@ -107,9 +104,9 @@ class account_invoice(models.Model):
                         (move_line.reconcile_partial_id and\
                          move_line.reconcile_partial_id.line_partial_ids):
                         raise except_orm(_('Error !'),
-                                         _('You can not reopen an invoice '\
-                                           'which is partially paid! You '\
-                                           'need to unreconcile related'\
+                                         _('You can not reopen an invoice '
+                                           'which is partially paid! You '
+                                           'need to unreconcile related '
                                            'payment entries first!'))
             self.write(cr, uid, i['id'], {'state': 'draft'})
             workflow.trg_delete(uid, 'account.invoice', i['id'], cr)
@@ -131,16 +128,17 @@ class account_invoice(models.Model):
                                ('datas_fname', '=', aname),
                                ('res_id', '=', inv.id)]
                         attachment_ids = attachment_obj.search(cr, uid, dmn)
+                        pdf1 = '.pdf.pdf'
                         for a in attachment_obj.browse(cr, uid,
                                                        attachment_ids):
                             vals = {
                                 'name': a.name.replace('.pdf', now + '.pdf'),
                                 'datas_fname':
-                            a.datas_fname.replace('.pdf.pdf', now + '.pdf.pdf')
-                                   }
+                                a.datas_fname.replace(pdf1, now + pdf1)
+                                    }
                             attachment_obj.write(cr, uid, a.id, vals)
 
-        # unset set the invoices move_id
+#        unset set the invoices move_id
         self.write(cr, uid, ids, {'move_id': False})
 
         if move_ids:
@@ -150,7 +148,7 @@ class account_invoice(models.Model):
                 _logger.debug('FGF reopen move_copy moveid %s' % (move.id))
                 if move.journal_id.entry_posted:
                     raise except_orm(_('Error !'),
-                                     _('You can not reopen an invoice '\
+                                     _('You can not reopen an invoice '
                                        'if the journal is set to skip draft!'))
                 move_copy_id = account_move_obj.copy(cr, uid, move.id)
                 _logger.debug('FGF reopen move_copy_id %s' % (move_copy_id))
@@ -172,9 +170,9 @@ class account_invoice(models.Model):
                 _logger.debug('FGF reopen reconcile_line_ids %s' % (line_ids))
                 lines_to_reconile = []
                 for ltr in account_move_line_obj.browse(cr, uid, line_ids):
-                    if ltr.account_id.id in\
-                    (ltr.partner_id.property_account_payable.id,
-                     ltr.partner_id.property_account_receivable.id):
+                    if ltr.account_id.id in(
+                            ltr.partner_id.property_account_payable.id,
+                            ltr.partner_id.property_account_receivable.id):
                         lines_to_reconile.append(ltr.id)
                 account_move_line_obj.write(cr, uid, lines_to_reconile,
                                             {'reconcile_id': r_id})
@@ -182,17 +180,17 @@ class account_invoice(models.Model):
         self._log_event(cr, uid, ids, -1.0, 'Reopened Invoice')
         return True
 
-    #def action_move_create(self, cr, uid, ids, context=None):
-    #    move_obj = self.pool.get('account.move')
-    #    move_ids = [] # ones that we will need to update
-    #    for inv in self.browse(cr, uid, ids, context=context):
-    #        if inv.move_id:
-    #            move_ids.append(inv.move_id.id)
-    #        else:
-    #            super(account_invoice, self).action_move_create(cr,
-    #                                        uid, ids, context)
-    #    if move_ids:
-    #        move_obj.write(cr, uid, move_ids, {'state':'posted'})
+#    def action_move_create(self, cr, uid, ids, context=None):
+#        move_obj = self.pool.get('account.move')
+#        move_ids = [] # ones that we will need to update
+#        for inv in self.browse(cr, uid, ids, context=context):
+#            if inv.move_id:
+#                move_ids.append(inv.move_id.id)
+#            else:
+#                super(account_invoice, self).action_move_create(cr,
+#                                            uid, ids, context)
+#        if move_ids:
+#            move_obj.write(cr, uid, move_ids, {'state':'posted'})
 
     @api.multi
     def action_number(self):
